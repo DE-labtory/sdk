6ea8962c6456090052eb720c29a9e90e2e6deccf
ParkHaeSung <hea9549@gmail.com>
2018-10-06 16:16:18 +0900

    haesung

--- cell.go
+++ cell.go
@@ -18,6 +18,9 @@
 package sdk
 
 import (
+	"errors"
+
+	"github.com/it-chain/sdk/logger"
 	"github.com/it-chain/sdk/pb"
 	"github.com/rs/xid"
 )
@@ -65,11 +68,53 @@ func (c Cell) PutData(key string, value string) error {
 }
 
 func (c Cell) GetData(key string) ([]byte, error) {
-	return value, err
+	uuid := xid.New().String()
+	mychan := make(chan stateRequestResult, 1)
+	c.requestMap[uuid] = mychan
+	err := c.serverStream.stream.Send(&pb.Message{
+		Message: &pb.Message_ReadStateRequest{
+			ReadStateRequest: &pb.ReadRequest{
+				Uuid:      uuid,
+				IcodeName: c.icodeName,
+				Key:       key,
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	for {
+		select {
+		case result := <-mychan:
+			return []byte(result.value), result.err
+		default:
+		}
+	}
 }
 
 func (c Cell) handleStateResult(message *pb.Message) {
-
+	switch x := message.Message.(type) {
+	case *pb.Message_ReadStateResponse:
+		getChan := c.requestMap[x.ReadStateResponse.Uuid]
+		if getChan == nil {
+			logger.Error(&logger.Fields{"uuid": x.ReadStateResponse.Uuid}, "no channel for handle read state response! ")
+			return
+		}
+		getChan <- stateRequestResult{
+			value: x.ReadStateResponse.Value,
+			err:   nil,
+		}
+	case *pb.Message_WriteStateResponse:
+		getChan := c.requestMap[x.WriteStateResponse.Uuid]
+		if getChan == nil {
+			logger.Error(&logger.Fields{"uuid": x.WriteStateResponse.Uuid}, "no channel for handle write state response! ")
+			return
+		}
+		getChan <- stateRequestResult{
+			value: x.WriteStateResponse.Value,
+			err:   errors.New(x.WriteStateResponse.Error.String()),
+		}
+	}
 }
 
 type stateRequestResult struct {
